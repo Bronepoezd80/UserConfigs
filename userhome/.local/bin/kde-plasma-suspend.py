be7270a2e7452f3182b28ab4cf9101d75f920e6f
Jakob Janzen <jakob.janzen80@gmail.com>
2023-01-03 16:01:15 +0100

    Update kde-plasma-suspend.py
    integrated functions into a class

--- userhome/.local/bin/kde-plasma-suspend.py
+++ userhome/.local/bin/kde-plasma-suspend.py
@@ -2,37 +2,54 @@
 """
     Jakob Janzen (me)
     jakob.janzen80@gmail.com
-    2023-01-01
+    2023-01-03
 
 """
 import os as _os
 import sys as _sys
 import subprocess as _subprocess
 
-def run_command(command_, nextline_=False):
-    output = _subprocess.run(command_, capture_output=True)
-    decoded = output.stdout.decode('utf-8')
-    if nextline_:
-        return decoded
-    return decoded.removesuffix('\n')
 
-def pactl_get_volume():
-    output = run_command(["pactl", "get-sink-volume", "0"])
-    print(output)
-    return
+class PACtl(object):
+    def __init__(self):
+        self.__command = ["pactl"]
+        self.output = None
 
-def pactl_set_volume(volume_=0):
-    output = run_command(["pactl", "set-sink-volume", "0", str(volume_)+"%"])
-    return
+    def __run(self, options_, nextline_=False):
+        """ Run command and capture output. """
+        self.__command.extend(options_)
+        self.output = _subprocess.run(self.__command, capture_output=True)
+        self.output = self.output.stdout.decode('utf-8')
+        if nextline_:
+            return self.output
+        return self.output.removesuffix('\n')
+
+    def get_volume(self):
+        """ Get global volume. """
+        print("current global volume:")
+        self.output = self.__run(["get-sink-volume", "0"])
+        print(self.output)
+        return
+
+    def set_volume(self, value_="0"):
+        """ Set global volume. """
+        value_ = str(value_)+"%"
+        print("setting global volume to {} ...".format(value_))
+        self.output = self.__run(["set-sink-volume", "0", value_])
+        return
+
 
 def main():
-    # Reset volume before set it.
-    pactl_get_volume()
-    pactl_set_volume()
-    pactl_get_volume()
-    pactl_set_volume(30)
-    pactl_get_volume()
+    PACtl().get_volume()
+
+    PACtl().set_volume()
+    PACtl().get_volume()
+
+    PACtl().set_volume(30)
+    PACtl().get_volume()
+
     return
+
 
 if __name__ == "__main__":
     main()
